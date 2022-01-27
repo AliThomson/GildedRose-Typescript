243c13d3a8198c896040510b0c65ed5db7920e66
Alison Thomson <Alison.Thomson@softwire.com>
2022-01-27 15:43:32 +0000

    Increase to special items passes all tests

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -18,58 +18,46 @@ export class GildedRose {
     }
 
     updateQuality() {
-        const specialItems = ['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert', 'Sulfuras, Hand of Ragnaros'];
-
+        
         for (let i = 0; i < this.items.length; i++) {
-            //for all items that are not 'special'
-            if (!specialItems.includes(this.items[i].name)) {
-
-                if (this.items[i].quality > 0) {
-                    this.items[i].quality = this.items[i].quality - 1
-                }
-            } else {
-                if (this.items[i].name !== 'Sulfuras, Hand of Ragnaros') {
-
-                    if (this.items[i].quality < 50) {
+                            
+            switch (this.items[i].name) {
+                case 'Aged Brie':
+                    if (this.items[i].sellIn <= 0) {
+                        this.items[i].quality = this.items[i].quality + 2
+                    } else {
                         this.items[i].quality = this.items[i].quality + 1
-                        if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-                            if (this.items[i].sellIn < 11) {
-                                if (this.items[i].quality < 50) {
-                                    this.items[i].quality = this.items[i].quality + 1
-                                }
-                            }
-                            if (this.items[i].sellIn < 6) {
-                                if (this.items[i].quality < 50) {
-                                    this.items[i].quality = this.items[i].quality + 1
-                                }
-                            }
-                        }
-                    } 
+                    }
+                    if (this.items[i].quality > 50) {
+                        this.items[i].quality = 50;
+                    }
+                break;
+                case 'Backstage passes to a TAFKAL80ETC concert':
+                    if (this.items[i].sellIn <= 0) {
+                        this.items[i].quality = 0
+                    } else if (this.items[i].sellIn <= 5) {
+                        this.items[i].quality = this.items[i].quality + 3
+                    } else if (this.items[i].sellIn <= 10) {
+                        this.items[i].quality = this.items[i].quality + 2
+                    } else {
+                        this.items[i].quality = this.items[i].quality + 1
+                    }
+                    if (this.items[i].quality > 50) {
+                        this.items[i].quality = 50;
+                    }
+                    break;
+                case 'Sulfuras, Hand of Ragnaros':
+                    //quality never changes
+                    break;
+                default:
+                    if (this.items[i].quality > 0) {
+                        this.items[i].quality = this.items[i].quality - 1
+                    }
                 }
-            }
-
             if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
                 this.items[i].sellIn = this.items[i].sellIn - 1;
             }
-            if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Aged Brie') {
-                    if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                                this.items[i].quality = this.items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality
-                    }
-                } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1
-                    }
-                }
-            }
         }
-
         return this.items;
     }
 }
